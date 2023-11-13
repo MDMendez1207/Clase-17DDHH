b9d5677f4df34f5829ea2a90cfd38fdddc5a9fa1
MDMarcos1207 <marcos.m1207@gmail.com>
2023-11-13 14:55:11 -0300

    MercadoLiebre deploy

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const path = require('path')
-
 const app = express()
+const port = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname,"public")));
 
-app.listen(3000, () => { 
-    console.log("Escucho")
+app.listen(port, () => { 
+    console.log(`Escucho en puerto ${port}`)
 })
 
 app.get("/", (req,res)=> {
